704cb9d03f6477c8ab812c963b897c74b3139b41
Zamorowski Dawid <d.zamorowski@ecs.local>
2023-03-16 14:08:31 +0100

    Próba z config do bazy Xampp

--- pages/config.js
+++ pages/config.js
@@ -1,18 +1,24 @@
-const mysql = require('mysql');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false })); // Remove 
+app.use(express.urlencoded({extended: true})); // New
+// Parse application/json
+// app.use(bodyParser.json()); // Remove
+app.use(express.json()); // New
 
 // Tworzenie połączenia z bazą danych
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'd4vez'
 });
 
-// Nawiązanie połączenia
-connection.connect((err) => {
-  if (err) {
-    console.error('Błąd połączenia z bazą danych: ' + err.stack);
-    return;
-  }
-  console.log('Połączono z bazą danych.');
-});
+app.listen(port, () => console.log(`Listening on port ${port}`))
